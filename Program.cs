386a911caa76216ba384c066c49018191d961995
Said Rodrigues <coderflemis@gmail.com>
2025-05-10 14:33:05 -0300

    feat: add serilog for structured logging

--- Program.cs
+++ Program.cs
@@ -1,3 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Serilog;
+using Microsoft.Extensions.Configuration;
 
-Console.WriteLine("Hello, World!");
+namespace YggdrasilVinum;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        // Configure Serilog
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        try
+        {
+            Log.Information("Starting B+ Tree Database Application");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
