05e8510be7f8323b122104ecfc31853c74f7afd8
Said Rodrigues <coderflemis@gmail.com>
2025-05-14 10:38:57 -0300

    feat: add LRU Buffer Manager

--- YggdrasilVinum/Buffer/IBufferManager.cs
+++ YggdrasilVinum/Buffer/IBufferManager.cs
@@ -7,6 +7,7 @@ public interface IBufferManager
     Task<Result<Unit, BufferError>> InitializeAsync();
 
     // Page operations
+    Task<Result<Page, BufferError>> GetRandomPageAsync();
     Task<Result<Page, BufferError>> LoadPageAsync(ulong pageId);
     Task<Result<Unit, BufferError>> PutPageAsync(Page page);
     Task<Result<Unit, BufferError>> FlushPageAsync(ulong pageId);
